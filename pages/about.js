85227ceae545301fa4a11dd12e2ee9f82a5ff6d5
Connor Detlefsen <Detlefsen@chapman.edu>
2021-12-09 18:09:39 -0800

    upload

---
+++ pages/about.js
@@ -0,0 +1,66 @@
+import Layout from "../Components/layout";
+export default function About() {
+  const handleSubmit = () => {
+    console.log("submit");
+  };
+
+  return (
+    <Layout home>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous"
+        />
+
+        <title>Hello, world!</title>
+      </head>
+      <section className="flex flex-col min-h-full flex-grow">
+        <h1 className="p-12 text-7xl text-center border-b-2 border-gray-300">
+          ABOUT
+        </h1>
+        <div className="flex flex-row flex-wrap items-center h-full border-b-2 border-gray-300">
+          <h1 className="px-12 text-5xl">Buying</h1>
+          <p className="text-left w-4/5 p-12 text-xl">
+            We created PartOut to curate listings for individual items so that
+            buyers can find used wheels at the lowest price possible. We’ve made
+            the buying process simple by eliminating the hassle of communicating
+            with the seller to figure out logistics, therefore, all you have to
+            do is purchase online and have your item arrive at your door.
+          </p>
+        </div>
+        <div className="flex flex-row flex-wrap h-full items-center border-b-2 border-gray-300	">
+          <h1 className="px-12 text-5xl">Selling</h1>
+          <p className="text-left w-4/5 p-12 text-xl">
+            PartOut aggregates buyers who are looking for the specific item you
+            are selling. Just search for your item, submit your listing, and
+            start selling. Once your item sells, ship your item using the
+            prepaid shipping label we send you. Once the buyer receives the
+            item, you get paid.
+          </p>
+        </div>
+        <div className="w-full flex flex-col items-center ">
+          <h1 className="text-center p-12 ">
+            Sign up to learn about news, updates, promotions, and more!
+          </h1>
+          <div className="flex flex-row">
+            <form>
+              <input
+                type="text"
+                placeholder="Enter your email address..."
+                className="h-12 w-64 p-2 rounded-md border-2	border-gray-300"
+              ></input>
+              <button
+                className="p-2 m-2 rounded-md border-2 border-gray-300 hover:text-white hover:bg-black"
+                onClick={handleSubmit}
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import Head from "next/head";
+import Header from "../Components/header";
+import Product from "../Components/product";
+import Layout from "../Components/layout";
+import Link from "next/link";
+import productContainer from "../Components/productContainer";
+import { Auth } from "@supabase/ui";
+import { useState } from "react";
+
+const ROUTE_PRODUCT_ID = "/products/[id]";
+
+export default function Home(allProductData) {
+  const [products, setProducts] = useState([
+    { id: 1, name: "tire", price: 110 },
+    { id: 2, name: "wheels", price: 250 },
+    { id: 3, name: "Car", price: 1000 },
+    { id: 4, name: "Tools", price: 10 },
+    { id: 5, name: "Mod", price: 400 },
+    { id: 6, name: "Car", price: 1000 },
+    { id: 7, name: "Wheel", price: 10 },
+    { id: 8, name: "Motorcycle Wheel", price: 400 },
+    { id: 9, name: "5mm Round Cut Tennis Bracelet in White Gold", price: 1000 },
+  ]);
+  const { user } = Auth.useUser();
+
+  return (
+    <Layout home>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous"
+        />
+
+        <title>Hello, world!</title>
+      </head>
+      <section>
+        <h1 className="p-12 text-7xl text-center border-b-2 border-gray-300">
+          PRODUCTS
+        </h1>
+
+        <div className="flex-row flex justify-center p-8 flex-wrap">
+          {products.map((product) => (
+            <div
+              key={`product-${product.id}`}
+              className=" shadow-md rounded-lg m-4 p-3 w-1/4 flex flex-col border-2  border-gray-300"
+            >
+              <Link
+                href={{
+                  pathname: ROUTE_PRODUCT_ID,
+                  query: {
+                    id: product.id,
+                    name: product.name,
+                    price: product.price,
+                  },
+                }}
+              >
+                <a className="text-4xl text-black py-2">{product.name}</a>
+              </Link>
+              <p>${product.price}</p>
+
+              <button className="bg-black text-white w-1/3 p-2 rounded-lg hover:bg-gray-600">
+                Add to cart
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
+/* old code for mapping dynamic routing
+ <ul className="flex flex-col items-center justify-center min-h-screen py-2">
+          {[allProductData].map(({ id, name }) => (
+            <li key={id}>
+              <Link href={`/products/${id}`}>
+                <a>{name}</a>
+              </Link>
+              <div>{id}</div>
+              <br />
+            </li>
+          ))}
+        </ul>
+*/
